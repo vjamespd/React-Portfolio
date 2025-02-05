5749ef91cebdf8e9643707eed4a1a301e12e9809
vjamespd <dvinardjames@gmail.com>
2025-02-05 18:02:58 +0800

    Fixed Experience and added a projects.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -18,10 +18,10 @@ const Experience = () => {
                     <p className="mb-4 text-neutral-400">{experience.description}</p>
                     {experience.technologies.map((tech, index) => (
                         <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">{tech}</span>
-                    ))};
+                    ))}
                 </div>
             </div>
-        ))};
+        ))}
       </div>
     </div>
   )
