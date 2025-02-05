2f05fac82c029bd847bb2efeab591146d52426f1
vjamespd <dvinardjames@gmail.com>
2025-02-06 02:45:07 +0800

    Updating correct infomations

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,52 +7,47 @@ export const HERO_CONTENT = `I am a passionate Software Engineer with a strong f
 expertise extends to UI/UX design principles and backend frameworks, ensuring user-friendly, visually appealing applications and
 providing a smooth user experience.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I am a fresh graduate from Adamson University with a bachelor degree in Computer Science, I have worked with a variety of technologies, including React, Express.js, Node.js, MySQL, Firebase, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "February - May 2024",
+    role: "Full Stack Developer - Intern",
+    company: "Humble Ripple Inc.",
+    description: `Developed and maintained the website server for MyFairee app, including designing and programming the website. Managed and
+created the database, as well as developed and maintained necessary APIs to ensure seamless functionality and integration.`,
+    technologies: [
+      "Javascript",
+      "HTML",
+      "CSS",
+      "Firebase",
+      "Figma",
+      "Flutter Flow",
+      "Canva"
+    ],
   },
   {
     year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    role: "Multimedia Specialist - Intern",
+    company: "Office of 6th District Congressman Bienvenido M. Abante",
+    description: `Official videographer, Photographer and Video Editor of the Office of 6th District Congressman Bienvenido M. Abante`,
+    technologies: ["Adobe Photoshop", "Adobe Premier", "Canva"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "B2B E-Commerce Website",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "A fully functional e-commerce website with features like product listing, shopping cart, admin dashboard, order management, order tracking, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB", "Express.js"],
   },
   {
-    title: "Task Management App",
+    title: "Chat App",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+      "An application for communication, with features such as user authentication, text messaging, push notifications, emoji and stickers, multimedia sharing, profile creation and contact list.",
     technologies: ["HTML", "CSS", "Angular", "Firebase"],
   },
   {
@@ -60,19 +55,19 @@ export const PROJECTS = [
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["HTML", "CSS", "React", "TailwindCSS", "Node.js"],
   },
   {
-    title: "Blogging Platform",
+    title: "Social Media App",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "A food-focused social media platform with features such as user profiles, recipe posting and sharing, recipe discovery, comments and feedbacks, likes and favorites and message application.",
+    technologies: ["HTML", "CSS", "JavaScript", "React", "Firebase"],
   },
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Kasilawan, Makati City, Metro Manila, Philippines ",
+  phoneNo: "+639451393719 ",
+  email: "dvinardjames@gmail.com",
 };
